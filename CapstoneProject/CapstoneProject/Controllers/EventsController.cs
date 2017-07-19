60af39dbb6a91728082cfc03522125f49a73f84d
etaylor113 <etaylor113@gmail.com>
2017-07-19 12:06:00 -0500

    added event controller and model

--- CapstoneProject/CapstoneProject/Controllers/EventsController.cs
+++ CapstoneProject/CapstoneProject/Controllers/EventsController.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace CapstoneProject.Controllers
 {
-    public class EventsController : Controller
+    public class EventsController : ApiController
     {
-        // GET: Event
-        public ActionResult ViewEvents()
-        {
-            return View();
-        }
     }
 }
