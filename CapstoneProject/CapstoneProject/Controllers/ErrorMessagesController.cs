2f9c59cb8f82ed2d36659ad3283d737fe73a4fc3
etaylor113 <etaylor113@gmail.com>
2017-07-21 15:35:49 -0500

    created error page for non Registered users

---
+++ CapstoneProject/CapstoneProject/Controllers/ErrorMessagesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CapstoneProject.Controllers
+{
+    public class ErrorMessagesController : Controller
+    {
+        public ActionResult RegisterAccessRequired()
+        {
+            return View();
+        }
+
+    }
+}
