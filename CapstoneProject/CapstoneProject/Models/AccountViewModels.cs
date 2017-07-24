6be653d5f1d84072b445e9f76e8b4e821482c7b3
etaylor113 <etaylor113@gmail.com>
2017-07-24 07:04:06 -0500

    added days of week to user db

--- CapstoneProject/CapstoneProject/Models/AccountViewModels.cs
+++ CapstoneProject/CapstoneProject/Models/AccountViewModels.cs
@@ -114,6 +114,27 @@ namespace CapstoneProject.Models
         [Display(Name = "*Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Monday")]
+        public string Monday { get; set; }
+
+        [Display(Name = "Tuesday")]
+        public string Tuesday { get; set; }
+
+        [Display(Name = "Wednesday")]
+        public string Wednesday { get; set; }
+
+        [Display(Name = "Thursday")]
+        public string Thursday { get; set; }
+
+        [Display(Name = "Friday")]
+        public string Friday { get; set; }
+
+        [Display(Name = "Saturday")]
+        public string Saturday { get; set; }
+
+        [Display(Name = "Sunday")]
+        public string Sunday { get; set; }
     }
 
     public class ResetPasswordViewModel
